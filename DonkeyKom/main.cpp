3a7b90fc5d7ecf74e511e92da2e93baa148cf685
Brandon Walker <brandon@branw.xyz>
2017-12-23 11:05:18 -0500

    Remove UAC elevation exploit

--- DonkeyKom/main.cpp
+++ DonkeyKom/main.cpp
@@ -2,17 +2,9 @@
 #include <iostream>
 #include <stdexcept>
 #include "dk/dk.hpp"
-#include "uac.hpp"
 
 int main(int argc, char *argv[]) {
 	try {
-		// Check process elevation and elevate as necessary using token manipulation
-		if (can_be_elevated()) {
-			std::cout << "Process is not fully elevated; attempting elevation" << std::endl;
-			elevate(GetCommandLineW());
-			return EXIT_SUCCESS;
-		}
-
 		std::cout << "Initializing DKOM" << std::endl;
 
 		dk::donkey_kom dkom;
--- DonkeyKom/uac.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <Windows.h>
-
-bool can_be_elevated();
-
-void elevate(LPWSTR cmd);
